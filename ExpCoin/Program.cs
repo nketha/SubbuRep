92ecbef1107698750f095b93cd748f4998bdd22f
nketha <nagasubrahmanyam.ketha@varsun.com>
2018-07-25 16:43:33 +0530

    create block
    create block

--- ExpCoin/Program.cs
+++ ExpCoin/Program.cs
@@ -1,5 +1,6 @@
 ﻿using NBitcoin;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ExpCoin
@@ -11,8 +12,14 @@ namespace ExpCoin
             decimal subbuCoin1 = 0;
             decimal subbuCoin2 = 0;
             decimal sendAmount = 0;
+            int blockCount = 0;
+            int option = 0;
 
             RandomUtils.Random = new UnsecureRandom();
+
+            List<Transaction> transactionList = new List<Transaction>();
+            DateTimeOffset current = DateTimeOffset.UtcNow;
+            ConcurrentChain chain = new ConcurrentChain(Network.Main);
 
             Key subbuPrivateKey = new Key(); Key swethaPrivateKey = new Key();
             Key adityaPrivateKey = new Key(); Key kavyaPrivateKey = new Key();
@@ -24,31 +31,68 @@ namespace ExpCoin
 
             Console.WriteLine("\n Enter first coin value:");
             subbuCoin1 = Convert.ToDecimal(Console.ReadLine());
+
             Console.WriteLine("\n Enter second coin value:");
             subbuCoin2 = Convert.ToDecimal(Console.ReadLine());
-            Console.WriteLine("\n Enter Send amount:");
-            sendAmount = Convert.ToDecimal(Console.ReadLine());
-            Transaction subbuFunding = new Transaction() {
-                Outputs =
+
+            decimal totalAmount = subbuCoin1 + subbuCoin2;
+            Console.WriteLine("\n Total Coins Value: " + totalAmount);
+
+            while (blockCount != 10) {
+
+                while (option != 1) {
+
+                    Console.WriteLine("\n Enter Send amount:");
+                    sendAmount = Convert.ToDecimal(Console.ReadLine());
+
+                    Transaction subbuFunding = new Transaction() {
+                        Outputs =
+
     {
+
         new TxOut(subbuCoin1.ToString(), subbu.GetAddress()),
         new TxOut(subbuCoin2.ToString(), subbu.PubKey)
     }
-            };
-            Coin[] subbuCoins = subbuFunding
-                                    .Outputs
-                                    .Select((o, i) => new Coin(new OutPoint(subbuFunding.GetHash(), i), o))
-                                    .ToArray();
-            var txBuilder = new TransactionBuilder();
-            var tx = txBuilder
-                .AddCoins(subbuCoins)
-                .AddKeys(subbu.PrivateKey)
-                .Send(swetha.GetAddress(), sendAmount.ToString())
-                .SendFees("0.001")
-                .SetChange(subbu.GetAddress())
-                .BuildTransaction(true);
-            Console.WriteLine(tx);
-            Console.ReadLine();
+                    };
+
+                    Coin[] subbuCoins = subbuFunding
+                                            .Outputs
+                                            .Select((o, i) => new Coin(new OutPoint(subbuFunding.GetHash(), i), o))
+                                            .ToArray();
+                    var txBuilder = new TransactionBuilder();
+
+                    var tx = txBuilder
+                        .AddCoins(subbuCoins)
+                        .AddKeys(subbu.PrivateKey)
+                        .Send(swetha.GetAddress(), sendAmount.ToString())
+                        .SendFees("0.001")
+                        .SetChange(subbu.GetAddress())
+                        .BuildTransaction(true);
+                    Console.WriteLine(tx);
+                    Console.ReadLine();
+                    transactionList.Add(tx);
+                    Console.WriteLine("Press 1 for Mine or 2 for next transaction:");
+                    option = int.Parse(Console.ReadLine());
+                }
+                chain.SetTip(CreateBlock(current, blockCount, transactionList, chain));
+                blockCount++;
+                option = 0;
+            }
+        }
+        private static ChainedBlock CreateBlock(DateTimeOffset current, int offset, List<Transaction> transactionList, ChainBase chain = null)
+        {
+            Block block = Consensus.Main.ConsensusFactory.CreateBlock();
+            if (chain != null) {
+
+                block.Header.HashPrevBlock = chain.Tip.HashBlock;
+                block.Transactions.AddRange(transactionList);
+                Console.WriteLine("Previous Block Hash" + block.Header.HashPrevBlock);
+                Console.WriteLine("Created Block Hash:" + block.GetHash());
+                Console.ReadLine();
+                return new ChainedBlock(block.Header, null, chain.Tip);
+            }
+            else
+                return new ChainedBlock(block.Header, 0);
         }
     }
 }
